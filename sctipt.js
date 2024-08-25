7b7c45a2e543bb34b5d6403c555952eece7f7906
Olesya Chaplinskaya <olesya@chaplinskaya.ru>
2024-08-25 19:28:14 +0300

    Добавлено: инструкция

--- sctipt.js
+++ sctipt.js
@@ -18,7 +18,7 @@ let flagAddHoriz = false;
 let spacingCenterX = 0;
 let spacingCenterY = 0;
 
-canvas.width = document.documentElement.clientWidth / 3 * 2;
+canvas.width = document.documentElement.clientWidth / 3 * 2 - 50;
 canvas.height = document.documentElement.clientHeight - 20;
 
 function float2int(value) {
@@ -1325,6 +1325,34 @@ function handleDocumentMouseUp(e) {
         handleMouseUp(e);
     }
 }
+
+// Get modal element
+var modal = document.getElementById("controlsModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("infoButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 document.addEventListener('mouseup', handleDocumentMouseUp); // Handle mouseup for the entire document
 
 // Инициализация при загрузке
